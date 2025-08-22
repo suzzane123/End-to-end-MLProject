c39260f9ab4041ff27f77f9642ee08efe5ecbc2d
suzzane123 <momsryder8@icloud.com>
2025-08-22 10:19:57 +0545

    changed

--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -12,7 +12,7 @@ class ConfigurationManager:
             self.config = read_yaml(config_filepath)
             self.params = read_yaml(params_filepath)
             self.schema = read_yaml(schema_filepath)
-            create_directories([self.config.artifact_root])
+            create_directories([self.config.artifacts_root])
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
